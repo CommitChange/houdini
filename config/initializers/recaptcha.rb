2949c9cd393f5dcc9537d90e615dc1bbdeac3204
Eric Schultz <eric@commitchange.com>
2021-09-15 11:38:14 -0500

    Remove unneeded recaptcha initializers

--- config/initializers/recaptcha.rb
+++
@@ -1,4 +0,0 @@
-Recaptcha.configure do |config|
-  config.api_server_url = 'https://www.google.com/recaptcha/api.js',
-  config.verify_url =   'https://recaptchaenterprise.googleapis.com/v1beta1/projects/307417/assessments'
-end
