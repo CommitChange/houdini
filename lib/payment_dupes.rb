b6f86dc02eaea4449824d00ca9fc1e885c09fda1
Clarissa Borges <clarissa@commitchange.com>
2022-02-10 20:38:15 -0300

    Moves the transaction block down to the loop

--- lib/payment_dupes.rb
+++ lib/payment_dupes.rb
@@ -60,22 +60,22 @@ module PaymentDupes
 
     def self.remove_payment_dupes(np_id, designations_to_become_comments)
         deleted_payments = []
-        ActiveRecord::Base.transaction do
-            nonprofit = Nonprofit.find(np_id)
-            etap_id_cf = CustomFieldMaster.find_by(name: 'E-Tapestry Id #').id
-            supp = nonprofit.supporters.not_deleted.joins(:custom_field_joins).where('custom_field_joins.custom_field_master_id = ?', etap_id_cf).references(:custom_field_joins)
-
-            supp.find_each do |s|
-                offsite_payments = s.payments.includes(:donation).where("kind = 'OffsitePayment'").joins(:journal_entries_to_item)
-                offsite_payments.find_each do |offsite|
-                    # match one offsite donation with an online donation if:
-                    # - the offsite donation was created on the same day that we ran the import and
-                    # - the offsite donation has the same date as the online payment
-                    # - there is a journal entry item for the offsite payment
-
-                    donation_or_ticket_payments = s.payments.not_matched.includes(:donation).where("(kind = 'Donation' OR kind = 'Ticket' OR kind = 'RecurringDonation') AND gross_amount = ? AND to_char(date, 'YYYY-MM-DD') = ?", offsite.gross_amount, offsite.date.strftime('%Y-%m-%d'))
-                    donation_or_ticket_payments.find_each do |online|
-                        reasons = []
+        nonprofit = Nonprofit.find(np_id)
+        etap_id_cf = CustomFieldMaster.find_by(name: 'E-Tapestry Id #').id
+        supp = nonprofit.supporters.not_deleted.joins(:custom_field_joins).where('custom_field_joins.custom_field_master_id = ?', etap_id_cf).references(:custom_field_joins)
+        
+        supp.find_each do |s|
+            offsite_payments = s.payments.includes(:donation).where("kind = 'OffsitePayment'").joins(:journal_entries_to_item)
+            offsite_payments.find_each do |offsite|
+                # match one offsite donation with an online donation if:
+                # - the offsite donation was created on the same day that we ran the import and
+                # - the offsite donation has the same date as the online payment
+                # - there is a journal entry item for the offsite payment
+                
+                donation_or_ticket_payments = s.payments.not_matched.includes(:donation).where("(kind = 'Donation' OR kind = 'Ticket' OR kind = 'RecurringDonation') AND gross_amount = ? AND to_char(date, 'YYYY-MM-DD') = ?", offsite.gross_amount, offsite.date.strftime('%Y-%m-%d'))
+                donation_or_ticket_payments.find_each do |online|
+                    reasons = []
+                    ActiveRecord::Base.transaction do
                         if online.kind == 'Ticket'
                             offsite.destroy
                             deleted_payments << offsite.id
@@ -102,49 +102,47 @@ module PaymentDupes
                             end
                             if reasons.none?
                                 if online.kind == 'RecurringDonation'
-                                    ActiveRecord::Base.transaction do
-                                        # addresses all the payments from that recurring donation so we avoid future problems
-                                        recurring_donation = online.donation
-                                        recurring_payments = recurring_donation.payments
-                                        temp_duplicate_payments = []
-                                        temp_offsite_matches = []
-                                        recurring_payments.find_each do |recurring_payment|
-                                            equivalent_offsite = s.payments.not_matched.where("kind = 'OffsitePayment' AND gross_amount = ? AND to_char(payments.date, 'YYYY-MM-DD') = ?", recurring_payment.gross_amount, recurring_payment.date.strftime('%Y-%m-%d')).joins(:journal_entries_to_item)
-                                            if equivalent_offsite.count == 1
-                                                # match!
-                                                temp_offsite_matches << equivalent_offsite.first.id
-                                                temp_duplicate_payments << equivalent_offsite.first.id.to_s
-                                                if recurring_payment.payment_dupe_status.present?
-                                                    recurring_payment.payment_dupe_status.matched = true
-                                                    recurring_payment.payment_dupe_status.matched_with_offline << equivalent_offsite.first.id
-                                                    recurring_payment.payment_dupe_status.save!
-                                                else
-                                                    recurring_payment.payment_dupe_status = PaymentDupeStatus.create!(matched: true, matched_with_offline: [equivalent_offsite.first.id])
-                                                end
-                                                if equivalent_offsite.first.payment_dupe_status.present?
-                                                    equivalent_offsite.first.payment_dupe_status.matched = true
-                                                    equivalent_offsite.first.payment_dupe_status.matched_with_offline << equivalent_offsite.first.id
-                                                    equivalent_offsite.first.payment_dupe_status.save!
-                                                else
-                                                    equivalent_offsite.first.payment_dupe_status = PaymentDupeStatus.create!(matched: true, matched_with_offline: [equivalent_offsite.first.id])
-                                                end
+                                    # addresses all the payments from that recurring donation so we avoid future problems
+                                    recurring_donation = online.donation
+                                    recurring_payments = recurring_donation.payments
+                                    temp_duplicate_payments = []
+                                    temp_offsite_matches = []
+                                    recurring_payments.find_each do |recurring_payment|
+                                        equivalent_offsite = s.payments.not_matched.where("kind = 'OffsitePayment' AND gross_amount = ? AND to_char(payments.date, 'YYYY-MM-DD') = ?", recurring_payment.gross_amount, recurring_payment.date.strftime('%Y-%m-%d')).joins(:journal_entries_to_item)
+                                        if equivalent_offsite.count == 1
+                                            # match!
+                                            temp_offsite_matches << equivalent_offsite.first.id
+                                            temp_duplicate_payments << equivalent_offsite.first.id.to_s
+                                            if recurring_payment.payment_dupe_status.present?
+                                                recurring_payment.payment_dupe_status.matched = true
+                                                recurring_payment.payment_dupe_status.matched_with_offline << equivalent_offsite.first.id
+                                                recurring_payment.payment_dupe_status.save!
+                                            else
+                                                recurring_payment.payment_dupe_status = PaymentDupeStatus.create!(matched: true, matched_with_offline: [equivalent_offsite.first.id])
+                                            end
+                                            if equivalent_offsite.first.payment_dupe_status.present?
+                                                equivalent_offsite.first.payment_dupe_status.matched = true
+                                                equivalent_offsite.first.payment_dupe_status.matched_with_offline << equivalent_offsite.first.id
+                                                equivalent_offsite.first.payment_dupe_status.save!
+                                            else
+                                                equivalent_offsite.first.payment_dupe_status = PaymentDupeStatus.create!(matched: true, matched_with_offline: [equivalent_offsite.first.id])
                                             end
                                         end
-                                        if temp_offsite_matches.any?
-                                            # it's the same donation for all of them so
-                                            # we can do the copies once
-                                            copy_comment(offsite, online)
-                                            copy_dedication(offsite, online)
-                                            copy_designation(offsite, online, designations_to_become_comments)
-                                            deleted_payments.concat(temp_duplicate_payments)
-                                            # deletes matching offsites here
-                                            p = Payment.find(temp_offsite_matches.first)
-                                            d = p.donation
-                                            d.payments.where(kind: 'OffsitePayment').destroy_all
-                                            d.destroy
-                                        else
-                                            raise ActiveRecord::Rollback
-                                        end
+                                    end
+                                    if temp_offsite_matches.any?
+                                        # it's the same donation for all of them so
+                                        # we can do the copies once
+                                        copy_comment(offsite, online)
+                                        copy_dedication(offsite, online)
+                                        copy_designation(offsite, online, designations_to_become_comments)
+                                        deleted_payments.concat(temp_duplicate_payments)
+                                        # deletes matching offsites here
+                                        p = Payment.find(temp_offsite_matches.first)
+                                        d = p.donation
+                                        d.payments.where(kind: 'OffsitePayment').destroy_all
+                                        d.destroy
+                                    else
+                                        raise ActiveRecord::Rollback
                                     end
                                 else
                                     copy_comment(offsite, online)
