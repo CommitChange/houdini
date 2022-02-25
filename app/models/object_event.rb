29eed6d2cb6a02718e98d4d390f8481615209ca4
Eric Schultz <eric@commitchange.com>
2022-02-24 21:56:50 -0600

    Use has_scope for the ObjectEventsController queries

--- app/models/object_event.rb
+++ app/models/object_event.rb
@@ -9,23 +9,13 @@ class ObjectEvent < ApplicationRecord
   belongs_to :nonprofit
 
   concerning :Query do
+
     class_methods do 
-      def query(params={})
-        result = where("TRUE") # just return everything you were previously going to return
-        if (params[:event_entity])
-          result = result.event_entity(params[:event_entity])
-        elsif (params[:event_types])
-          result = result.event_types(*params[:event_types])
-        end
-
-        result
-      end
-
       def event_entity(event_entity_houid)
         where(event_entity_houid:event_entity_houid)
       end
 
-      def event_types(*types)
+      def event_types(types)
         where('event_type IN (?)', types)
       end
     end
