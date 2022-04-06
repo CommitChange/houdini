ac3ba75abd78a8f1511dde64e20f16c7ae0bcd51
Eric Schultz <eric@commitchange.com>
2022-04-06 15:34:38 -0500

    spec/models/dispute_spec.rb passes

--- spec/models/dispute_spec.rb
+++ spec/models/dispute_spec.rb
@@ -6,18 +6,18 @@ RSpec.describe Dispute, :type => :model do
   it {is_expected.to have_one(:stripe_dispute).with_primary_key(:stripe_dispute_id).with_foreign_key(:stripe_dispute_id)}
 
   describe '.activities' do 
-    shared_context :common_specs do
-      let(:activity_json) { activity.json_data}
-      specify { expect(activity.supporter).to eq supporter}
-      specify { expect(activity.nonprofit).to eq nonprofit}
-      specify { expect(activity_json['status']).to eq dispute.status }
-      specify { expect(activity_json['reason']).to eq dispute.reason }
-      specify { expect(activity_json['original_id']).to eq charge.payment.id}
-      specify { expect(activity_json['original_kind']).to eq charge.payment.kind}
-      specify { expect(activity_json['original_gross_amount']).to eq charge.payment.gross_amount}
-      specify { expect(activity_json['original_date'].to_time).to eq charge.payment.date}
-      specify { expect(activity_json['gross_amount']).to eq dispute.gross_amount}
-    end
+    # shared_context :common_specs do
+    #   let(:activity_json) { activity.json_data}
+    #   specify { expect(activity.supporter).to eq supporter}
+    #   specify { expect(activity.nonprofit).to eq nonprofit}
+    #   specify { expect(activity_json['status']).to eq dispute.status }
+    #   specify { expect(activity_json['reason']).to eq dispute.reason }
+    #   specify { expect(activity_json['original_id']).to eq charge.payment.id}
+    #   specify { expect(activity_json['original_kind']).to eq charge.payment.kind}
+    #   specify { expect(activity_json['original_gross_amount']).to eq charge.payment.gross_amount}
+    #   specify { expect(activity_json['original_date'].to_time).to eq charge.payment.date}
+    #   specify { expect(activity_json['gross_amount']).to eq dispute.gross_amount}
+    # end
     
 
     describe "dispute.created" do
@@ -448,26 +448,241 @@ RSpec.describe Dispute, :type => :model do
         )
       }
 
-
-  
-      
-    #   # let(:obj) { StripeDispute.create(object:json) }
-    #   # let(:activity) { dispute.activities.build('DisputeWon', Time.at(event_json.created)) }
-
-    #   # specify { expect(activity.kind).to eq 'DisputeWon' }
-    #   # specify { expect(activity.date).to eq Time.at(event_json.created) }
     end
 
     describe "dispute.lost" do
-      include_context :common_specs
-      include_context :dispute_lost_context
-      
-      let(:obj) { StripeDispute.create(object:json) }
-      let(:activity) { obj.dispute.activities.build('DisputeLost', Time.at(event_json.created))}
+      class DisputeCaseLost
+        include FactoryBot::Syntax::Methods
+  
+        def dispute_on_stripe
+          StripeMockHelper.start
+          event_json = StripeMock.mock_webhook_event('charge.dispute.closed-lost')
+          StripeMockHelper.stripe_helper.upsert_stripe_object(:dispute, event_json['data']['object'])
+          @dispute_on_stripe ||= event_json
+        end
+  
+        def legacy_donation
+          fee_total = 0
+          gross_amount = 80000
+          stripe_charge_id = "ch_1Y7zzfBCJIIhvMWmSiNWrPAC"
+          date = Time.at(1596429794) - 1.day
+            @legacy_donation ||= create(:donation_base, amount: gross_amount, supporter:supporter,  nonprofit: nonprofit, payment: 
+                build(
+                  :payment_base, gross_amount:gross_amount, fee_total: fee_total, net_amount: gross_amount + fee_total,
+                  date: date, supporter:supporter,  nonprofit: nonprofit,
+                  charge: build(:charge_base, amount: gross_amount, stripe_charge_id: stripe_charge_id, created_at: date, supporter:supporter,  nonprofit: nonprofit,)
+                )
+            )   
+        end
+  
+        def legacy_payment
+          legacy_donation.payment
+        end
+  
+        def nonprofit
+          @supporter.nonprofit
+        end
+  
+        def supporter
+          @supporter ||= create(:supporter_base)
+        end
+  
+        def transaction_to_be_disputed
+          date = Time.at(1596429794) - 1.day
+          fee_total = 0
+          gross_amount = 80000
+          stripe_charge_id = "ch_1Y7zzfBCJIIhvMWmSiNWrPAC"
+          @transaction ||= build(:transaction_base,
+            created: date,
+            amount: gross_amount,
+            supporter: supporter,
+            legacy_donation: legacy_donation
+            )
+  
+            @transaction.save!
+          @transaction
+        end
+          
+        def stripe_dispute
+          event_json = dispute_on_stripe
+          @stripe_dispute ||= StripeDispute.create(object: event_json['data']['object'])
+        end
+  
+        def legacy_dispute
+          @legacy_dispute ||= stripe_dispute.dispute
+        end
 
-      specify { expect(activity.kind).to eq 'DisputeLost'}
-      specify { expect(activity_json['gross_amount']).to eq dispute.gross_amount}
-      specify { expect(activity.date).to eq Time.at(event_json.created)}
+        def withdrawal_dispute_transaction
+          legacy_dispute.dispute_transactions.order("date").first
+        end
+
+        def reinstated_dispute_transaction
+          legacy_dispute.dispute_transactions.order("date").second
+        end
+
+        def withdrawal_dispute_legacy_payment
+          legacy_dispute.dispute_transactions.order("date").first.payment
+        end
+
+        def reinstated_dispute_legacy_payment
+          legacy_dispute.dispute_transactions.order("date").second.payment
+        end
+
+        def transaction_payment_charge
+          transaction_to_be_disputed.reload
+          transaction_to_be_disputed.ordered_payments.last
+        end
+
+        def transaction_payment_withdrawal
+          transaction_to_be_disputed.reload
+          case transaction_to_be_disputed.ordered_payments.count
+          when 2
+            transaction_to_be_disputed.ordered_payments.first
+          when 3
+            transaction_to_be_disputed.ordered_payments.second
+          else
+            nil
+          end
+        end
+
+        def transaction_payment_reinstated
+          transaction_to_be_disputed.reload
+          case transaction_to_be_disputed.ordered_payments.count
+          when 3
+             transaction_to_be_disputed.ordered_payments.first
+          else
+            nil
+          end
+        end
+  
+        def setup
+          dispute_on_stripe
+          supporter
+          nonprofit
+          legacy_donation
+          transaction_to_be_disputed
+          stripe_dispute
+          self
+        end
+      end
+
+      it {
+        config = DisputeCaseLost.new.setup
+        
+        transaction = config.transaction_to_be_disputed
+        
+        legacy_dispute = config.stripe_dispute.dispute
+        
+        activities = config.legacy_dispute.activities
+
+        expect(activities).to include {
+            kind:"DisputeLost", 
+            date: Time.at(config.event_json.created),
+            supporter: transaction.supporter,
+            nonprofit: transaction.nonprofit,
+            json_data: include_json(
+              status: legacy_dispute.status,
+              reason: legacy_dispute.reason,
+              original_id: legacy_dispute.charge.payment.id,
+              original_kind: legacy_dispute.payment.kind,
+              original_gross_amount: legacy_dispute.payment.gross_amount,
+              original_date: config.charge.payment.date.to_time,
+              gross_amount: legacy_dispute.gross_amount
+            )
+
+        }
+      }
+      
+
+      it {
+        config = DisputeCaseLost.new.setup
+        expect(config.stripe_dispute).to have_attributes(
+          'status'=> 'lost',
+          "reason" => 'duplicate',
+          "balance_transactions" => an_instance_of(Array).and(have_attributes(count:1)),
+          "net_change" => -81500,
+          "amount" => 80000,
+          "stripe_charge_id" => "ch_1Y7zzfBCJIIhvMWmSiNWrPAC",
+          "stripe_dispute_id" => "dp_05RsQX2eZvKYlo2C0FRTGSSA",
+          "started_at" => Time.at(1596429794),
+        )
+      }
+
+      it {
+        config = DisputeCaseLost.new.setup
+        expect(config.legacy_dispute).to be_persisted
+      }
+
+      it {
+        config = DisputeCaseLost.new.setup
+        expect(config.legacy_dispute).to have_attributes( 
+        'status'=> 'lost',
+        "reason" => 'duplicate',
+        "gross_amount" => 80000,
+        "stripe_dispute_id" => "dp_05RsQX2eZvKYlo2C0FRTGSSA",
+        "started_at" => Time.at(1596429794)
+        )
+      }
+
+      it {
+        config = DisputeCaseLost.new.setup
+        config.transaction_to_be_disputed.reload
+        expect(config.transaction_to_be_disputed.payments.count).to eq 2
+      }
+
+      it {
+        config = DisputeCaseLost.new.setup
+        withdrawal_dispute_transaction = config.withdrawal_dispute_transaction
+        expect(withdrawal_dispute_transaction).to be_persisted
+      }
+
+      it {
+        config = DisputeCaseLost.new.setup
+        withdrawal_dispute_transaction = config.withdrawal_dispute_transaction
+        expect(withdrawal_dispute_transaction).to have_attributes(
+          gross_amount: -80000,
+          fee_total: -1500,
+          stripe_transaction_id: 'txn_1Y7pdnBCJIIhvMWmJ9KQVpfB',
+          date: DateTime.new(2020, 8, 3, 4, 55, 55),
+          disbursed: false
+        )
+      }
+
+      it {
+        config = DisputeCaseLost.new.setup
+        withdrawal_payment = config.withdrawal_dispute_legacy_payment
+        expect(withdrawal_payment).to be_persisted
+      }
+
+
+      it {
+        config = DisputeCaseLost.new.setup
+        withdrawal_payment = config.withdrawal_dispute_legacy_payment
+        expect(withdrawal_payment).to have_attributes(
+          gross_amount: -80000,
+          fee_total: -1500,
+          net_amount: -81500,
+          kind: "Dispute",
+          nonprofit: config.nonprofit,
+          date: DateTime.new(2020, 8, 3, 4, 55, 55)
+        )
+      }
+
+      it {
+        config = DisputeCaseLost.new.setup
+        reinstated_dispute_transaction = config.reinstated_dispute_transaction
+        expect(reinstated_dispute_transaction).to be_nil
+      }
+
+      
+      
+      
+      # let(:obj) { StripeDispute.create(object:json) }
+      # let(:activity) { obj.dispute.activities.build('DisputeLost', Time.at(event_json.created))}
+
+      # specify { expect(activity.kind).to eq 'DisputeLost'}
+      # specify { expect(activity_json['gross_amount']).to eq dispute.gross_amount}
+      # specify { expect(activity.date).to eq Time.at(event_json.created)}
     end
   end
 end
