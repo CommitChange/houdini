f3eef57b5012cad06195d4696e2aa7cee4100629
Clarissa Borges <clarissa@commitchange.com>
2022-05-12 12:27:46 -0300

    Fix similarity index added to name search migration

--- db/migrate/20220221223739_add_similarity_to_name_search.rb
+++ db/migrate/20220221223739_add_similarity_to_name_search.rb
@@ -1,7 +1,8 @@
 class AddSimilarityToNameSearch < ActiveRecord::Migration
   def change
     enable_extension :pg_trgm
+    enable_extension :btree_gin
 
-    add_index :supporters, :name, name: :name_search_idx, order: {name: :gin_trgm_ops}
+    add_index :supporters, :name, name: :name_search_idx, using: :gin, order: {name: :gin_trgm_ops}
   end
 end
