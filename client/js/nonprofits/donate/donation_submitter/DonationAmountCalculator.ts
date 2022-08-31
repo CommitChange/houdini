1b718166d4a99c010b7c6b80c81dfa8f2e61952e
Eric Schultz <eric@commitchange.com>
2022-08-31 13:47:00 -0500

    Move donation amount calculation into its own file

---
+++ client/js/nonprofits/donate/donation_submitter/DonationAmountCalculator.ts
@@ -0,0 +1,61 @@
+// License: LGPL-3.0-or-later
+
+import {CommitchangeFeeCoverageCalculator} from '../../../../../javascripts/src/lib/payments/commitchange_fee_coverage_calculator'
+
+export default class DonationAmountCalcuator implements EventTarget {
+ 
+  private _coverFees: boolean = false;
+  private _inputAmount:number = 0;
+  private eventTarget = new EventTarget();
+  
+  constructor(readonly feeStructure:{   flatFee:number; percentageFee:number}) {
+  }
+
+  get coverFees() {
+    return this._coverFees
+  }
+
+  set coverFees(val:boolean) {
+    const prev = this._coverFees
+    if (prev !== val) {
+      this._coverFees = val;
+      this.dispatchEvent(new Event('updated'));
+    }
+  }
+
+  get inputAmount() {
+    return this._inputAmount;
+  }
+
+  set inputAmount(val:number) {
+    if (val !== this._inputAmount) {
+      this._inputAmount = val;
+      this.dispatchEvent(new Event('updated'));
+    }
+  }
+
+  get calcResult(): {donationTotal:number, potentialFees:string} {
+    const calc  = new CommitchangeFeeCoverageCalculator({
+      ...this.feeStructure,
+      currency: 'usd',
+      feeCovering: this.coverFees
+    });
+
+    const calcFromNetResult = calc.calcFromNet(this._inputAmount);
+
+    return {
+      donationTotal: calcFromNetResult.actualTotalAsNumber,
+      potentialFees: calcFromNetResult.estimatedFees.feeAsString
+    }
+  }
+
+  addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void {
+    this.eventTarget.addEventListener(type, listener, options);
+  }
+  dispatchEvent(evt: Event): boolean {
+    return this.eventTarget.dispatchEvent(evt);
+  }
+  removeEventListener(type: string, listener?: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void {
+    this.eventTarget.removeEventListener(type, listener, options);
+  }
+}
