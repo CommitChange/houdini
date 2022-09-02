99fcd7349de9eeb705183aebbde52b9a15d557d8
Eric Schultz <eric@commitchange.com>
2022-09-02 15:35:52 -0500

    Move and rename DonationSubmitterEvents to EventStack

---
+++ client/js/nonprofits/donate/donation_submitter/EventStack.ts
@@ -0,0 +1,26 @@
+import last from 'lodash/last';
+import isEqual from 'lodash/isEqual'
+
+
+export default class EventStack<TEventObjects extends {type:string}> {
+
+  private events:Array<TEventObjects> = []
+  
+  push(event: TEventObjects): TEventObjects | undefined {
+    if (!this.top) {
+      this.events.push(event)
+      return event;
+    }
+    else if (!isEqual(this.top, event)) {
+      this.events.push(event);
+      return event;
+    }
+    return undefined;
+  }
+
+  get top(): TEventObjects | undefined {
+    return last(this.events);
+  }
+
+
+}
