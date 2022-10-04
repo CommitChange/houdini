5beac682aa4124eb257e6b0782a37cf38d994b99
Eric Schultz <eric@commitchange.com>
2022-10-04 15:19:28 -0500

    Move javascripts to app/javascript/legacy_react

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,7 @@
 const { environment } = require('@rails/webpacker')
 
+const babel = environment.loaders.get('babel')
+
+babel.test = /\.(js|jsx|mjs|ts|tsx|es6)?(\.erb)?$/
+
 module.exports = environment
