f50bc7ca5681efac9af6f1dc50352b81ca48a817
Yasmeen Awad <yasmeen@softwareforgood.com>
2023-03-07 13:29:58 -0600

    fix context file

--- spec/support/contexts/payout_context.rb
+++ spec/support/contexts/payout_context.rb
@@ -3,13 +3,12 @@
 # License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
 # Full license explanation at https://github.com/houdiniproject/houdini/blob/main/LICENSE
 
-shared_examples 'payout that can create object events with #publish_created'
+shared_examples 'payout that can create object events with #publish_created' do
+  let(:instance) { subject }
 
-    let!(:instance) { subject }
-
-    it {
-        initial_count = ObjectEvent.all.count
-        instance.publish_created
-        expect(ObjectEvent.all.count).to eq(initial_count + 1)
+  it {
+    initial_count = ObjectEvent.all.count
+    instance.publish_created
+    expect(ObjectEvent.all.count).to eq(initial_count + 1)
     }
 end
