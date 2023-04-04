6e67e17e4afa67052057e2479dbf6e9f1428d282
Eric Schultz <eric@commitchange.com>
2023-04-04 16:42:27 -0500

    Remove unused `InsertBillingSubscriptions.check_trial`

--- spec/lib/insert/insert_billing_subscriptions_spec.rb
+++ spec/lib/insert/insert_billing_subscriptions_spec.rb
@@ -14,22 +14,4 @@ describe InsertBillingSubscriptions, :skip => true do
       expect(sub["id"]).to be_present
     end
   end
-
-  describe '.check_trial' do
-
-    it 'marks as inactive after 10 days' do
-      sub
-      Timecop.freeze(10.days.from_now){ InsertBillingSubscriptions.check_trial(sub['id']) }
-      updated = Qx.fetch(:billing_subscriptions, sub['id']).last
-      expect(updated['status']).to eq('inactive')
-    end
-
-    it 'does not change the status if not still trialing after 10 days' do
-      sub
-      Qx.update(:billing_subscriptions).set(status: 'active').where("id = $id", id: sub['id']).execute
-      Timecop.freeze(10.days.from_now){ InsertBillingSubscriptions.check_trial(sub['id']) }
-      updated = Qx.fetch(:billing_subscriptions, sub['id']).last
-      expect(updated['status']).to eq('active')
-    end
-  end
 end
