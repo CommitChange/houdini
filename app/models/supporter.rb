763c83313c5db40dd13d72b74acd0445d65f1344
Yasmeen Awad <yasmeen@softwareforgood.com>
2023-04-19 10:48:25 -0500

    add object events to supporter, thru nonprofit

--- app/models/supporter.rb
+++ app/models/supporter.rb
@@ -66,6 +66,7 @@ class Supporter < ActiveRecord::Base
   has_many :activities, dependent: :destroy
   has_many :tickets
   has_many :recurring_donations
+  has_many :object_events, through: :nonprofit, source: :associated_object_events
 
   concerning :Tags do
     included do
