36585482fd5989e6da2c198af94abc08d246fe48
Yasmeen Awad <yasmeen@softwareforgood.com>
2023-05-10 13:15:58 -0500

    add invalid email address case in unit tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,36 +17,59 @@ RSpec.describe User, :type => :model do
 
   describe '.send_reset_password_instructions' do
 
-    context 'when the user hasn\'t requested a reset password token before' do
-      it 'returns a User' do
-        create(:user, email: 'evil_hacker@netflix.io')
-        expect(User.send_reset_password_instructions(attributes={email: 'evil_hacker@netflix.io'})).to be_a User
+    context 'with a valid email address' do
+
+      context 'when the user hasn\'t requested a reset password token before' do
+        it 'returns a User' do
+          create(:user, email: 'evil_hacker@netflix.io')
+          expect(User.send_reset_password_instructions(attributes={email: 'evil_hacker@netflix.io'})).to be_a User
+        end
+
+        it 'returns the correct user' do
+          user = create(:user, email: 'sneaky_scammer@fb.net')
+          expect(User.send_reset_password_instructions(attributes={email: 'sneaky_scammer@fb.net'})).to eq(user)
+        end
+
+        it 'returns an object with no errors' do
+          create(:user, email: 'gone_phishing@twtr.gov')
+          expect(User.send_reset_password_instructions(attributes={email: 'gone_phishing@twtr.gov'}).errors.messages).to eq({})
+        end
       end
 
-      it 'returns the correct user' do
-        user = create(:user, email: 'sneaky_scammer@fb.net')
-        expect(User.send_reset_password_instructions(attributes={email: 'sneaky_scammer@fb.net'})).to eq(user)
-      end
+      context 'when a user has requested a reset password token recently (< 5 min ago)' do
+        it 'returns the correct user' do
+          user = create(:user, email: 'fraud_fool@insta.org')
+          User.send_reset_password_instructions(attributes={email: 'fraud_fool@insta.org'})
+          expect(User.send_reset_password_instructions(attributes={email: 'fraud_fool@insta.org'})).to eq(user)
+        end
 
-      it 'returns an object with no errors' do
-        create(:user, email: 'gone_phishing@twtr.gov')
-        expect(User.send_reset_password_instructions(attributes={email: 'gone_phishing@twtr.gov'}).errors.messages).to eq({})
+        it 'adds "can\'t reset password because a request was just sent" error to returned user' do
+          create(:user, email: 'perilous_programmer@snap.com')
+          User.send_reset_password_instructions(attributes={email: 'perilous_programmer@snap.com'})
+          expect(
+            User.send_reset_password_instructions(attributes={email: 'perilous_programmer@snap.com'}).errors.messages[:user]
+          ).to eq(["can't reset password because a request was just sent"])
+        end
       end
     end
 
-    context 'when a user has requested a reset password token recently (< 5 min ago)' do
-      it 'returns the correct user' do
-        user = create(:user, email: 'fraud_fool@insta.org')
-        User.send_reset_password_instructions(attributes={email: 'fraud_fool@insta.org'})
-        expect(User.send_reset_password_instructions(attributes={email: 'fraud_fool@insta.org'})).to eq(user)
+    context 'with an invalid email address' do
+      it 'returns a User' do
+        expect(
+          User.send_reset_password_instructions(attributes={email: 'unknown_goose@domain.net'})
+        ).to be_a User
       end
 
-      it 'adds errors to returned user' do
-        create(:user, email: 'perilous_programmer@snap.com')
-        User.send_reset_password_instructions(attributes={email: 'perilous_programmer@snap.com'})
+      it 'returns a blank User' do
         expect(
-          User.send_reset_password_instructions(attributes={email: 'perilous_programmer@snap.com'}).errors.messages[:user]
-        ).to eq(["can't reset password because a request was just sent"])
+          User.send_reset_password_instructions(attributes={email: 'unknown_panda@domain.net'}).id
+        ).to be_nil
+      end
+
+      it 'adds "not found" errors to email' do
+        expect(
+          User.send_reset_password_instructions(attributes={email: 'unknown_lizard@domain.net'}).errors.messages[:email]
+        ).to eq(["not found"])
       end
     end
   end
