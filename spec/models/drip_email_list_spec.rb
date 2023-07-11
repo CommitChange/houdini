bd723112263718b154fa4a55434c0e7b2927adb5
Chelsey <chelsey@softwareforgood.com>
2023-07-11 11:21:04 -0500

    testing spec fix

--- spec/models/drip_email_list_spec.rb
+++ spec/models/drip_email_list_spec.rb
@@ -1,7 +1,7 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 require 'rails_helper'
 
-RSpec.describe Dripemaillist, type: :model do
+RSpec.describe DripEmailList, type: => :model do
 
   describe '#list_path' do
     it 'is correctly generated' do
