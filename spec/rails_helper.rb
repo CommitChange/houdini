53c235a76ba923759a0ee15f0bb51ca9b5194384
Eric Schultz <eric@commitchange.com>
2025-03-13 19:25:07 -0500

    Update database_cleaner gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -77,8 +77,19 @@ RSpec.configure do |config|
   config.include ActionMailerMatchers
   config.before(:suite) do
 
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation, reset_ids: true)
+    if config.use_transactional_fixtures?
+      raise(<<-MSG)
+        Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
+        (or set it to false) to prevent uncommitted transactions being used in
+        JavaScript-dependent specs.
+
+        During testing, the app-under-test that the browser driver connects to
+        uses a different database connection to the database connection used by
+        the spec. The app's database connection would not be able to access
+        uncommitted transaction data setup over the spec's database connection.
+      MSG
+    end
+    DatabaseCleaner.clean_with(:truncation)
     Rails.cache.clear
   end
 
