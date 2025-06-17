6119bfd1d49ad72eb45d8815d25afa6cef515bf4
Casey Helbling <casey@softwareforgood.com>
2025-06-17 12:06:38 -0500

    Tweak UI to add number of each event and whitespace between each block

--- client/js/events/listing-item/index.js
+++ client/js/events/listing-item/index.js
@@ -19,25 +19,30 @@ const dateTime = (startTime, endTime) => {
   ]
 }
 
-const metric = (label, val) => 
+const metric = (label, val) =>
   h('span.u-inlineBlock.u-marginRight--20', [h('strong', `${label}: `), val || '0'])
 
-const row = (icon, content) => 
+const checkedInPercent = (checkedIn = 0 , total = 0) => {
+  if (total === 0) return 0;
+  return Math.round((checkedIn * 100) / total);
+}
+
+const row = (icon, content) =>
   h('tr', [
     h('td.u-centered', [h(`i.fa.${icon}`)])
   , h('td.u-padding--10', content)
   ])
 
-module.exports = e => {
+module.exports = (e, key) => {
   const path = `/nonprofits/${app.nonprofit_id}/events/${e.id}`
   const location = [
-    h('p.strong.u-margin--0', e.venue_name) 
+    h('p.strong.u-margin--0', e.venue_name)
   , h('p.u-margin--0', commaJoin([e.address, e.city, e.state_code, e.zip_code]))
   ]
   const attendeesMetrics = [
-    metric('Attendees', e.total_attendees) 
-  , metric('Checked In', e.checked_in_count) 
-  , metric('Percent Checked In', Math.round((e.checked_in_count || 0) * 100 / (e.total_attendees || 0)) + '%')
+    metric('Attendees', e.total_attendees)
+  , metric('Checked In', e.checked_in_count)
+  , metric('Percent Checked In', checkedInPercent(e.checked_in_count, e.total_attendees) + '%')
   ]
   const moneyMetrics = [
     metric('Ticket Payments', '$' + format.centsToDollars(e.tickets_total_paid))
@@ -48,9 +53,10 @@ module.exports = e => {
     h('a.u-marginRight--20', {props: {href: path, target: '_blank'}}, 'Event Page')
   , h('a', {props: {href: path + '/tickets', target: '_blank'}}, 'Attendees Page')
   ]
-  return h('div.u-paddingTop--10.u-marginBottom--20', [
-    h('h5.u-paddingX--20', e.name)
-  , h('table.table--striped.u-margin--0', [
+
+  return h('div.u-paddingTop--10.u-paddingBottom--10', [
+    h(`h5.u-padding--20.u-margin--0.fundraiser--${key}`, e.name)
+  , h(`table.table--striped.u-margin--0.fundraiser--${key}`, [
       row('fa-clock-o', dateTime(e.start_datetime, e.end_datetime))
     , row('fa-map-marker', location)
     , row('fa-users', attendeesMetrics)
