dc3ac5a3358c453ed26d19358493fcc12bad96e9
Eric Schultz <eric@commitchange.com>
2025-06-18 16:06:40 -0500

    Adjust to use the `_base` versions of factories

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -1,12 +1,12 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 FactoryBot.define do
   factory :ticket do
-    event_discount
-    supporter
+    event_discount { association :event_discount_base }
+    supporter { association :supporter_base }
     profile
-    ticket_level
-    charge
-    payment
+    ticket_level { association :source_token_base }
+    charge { association :charge_base }
+    payment { association :payment_base }
     source_token
     ticket_purchase
     note { Faker::Lorem.sentence }
