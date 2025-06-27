01606973856d3652864ac37094d33ad3e8dee028
Eric Schultz <eric@commitchange.com>
2025-06-27 16:46:42 -0500

    Remove more requires

--- app/legacy_lib/query_supporters.rb
+++ app/legacy_lib/query_supporters.rb
@@ -1,6 +1,4 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
-require "qexpr"
-require "email"
 
 module QuerySupporters
   # Query supporters and their donations and gift levels for a campaign
