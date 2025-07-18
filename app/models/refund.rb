677c645e359a88c8db6ce5cdc97c0c1e9b3f8245
Eric Schultz <eric@commitchange.com>
2025-07-18 18:01:29 -0500

    Remove unneeded mass assignment protection on Refund

--- app/models/refund.rb
+++ app/models/refund.rb
@@ -2,17 +2,6 @@
 class Refund < ApplicationRecord
   Reasons = [:duplicate, :fraudulent, :requested_by_customer]
 
-  attr_accessible \
-    :amount, # int
-    :comment, # text
-    :reason, # str ('duplicate', 'fraudulent', or 'requested_by_customer')
-    :stripe_refund_id,
-    :disbursed, # boolean (whether this refund has been counted in a payout)
-    :failure_message, # str (accessor for storing the Stripe error message)
-    :user_id, :user, # user who made this refund
-    :payment_id, :payment, # negative payment that records this refund
-    :charge_id, :charge
-
   attr_accessor :failure_message
 
   belongs_to :charge
