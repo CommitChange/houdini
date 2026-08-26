d7012142049d1511aca22d8f4da89d3f09e5c2e0
Eric Schultz <eric@commitchange.com>
2026-08-25 19:05:11 -0500

    Fix build issues with missing encryption keys

--- config/application.rb
+++ config/application.rb
@@ -94,9 +94,9 @@ module Commitchange
 
     config.active_job.queue_adapter = :delayed_job
 
-    config.active_record.encryption.primary_key = ENV.fetch("ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY")
-    config.active_record.encryption.deterministic_key = ENV.fetch("ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY")
-    config.active_record.encryption.key_derivation_salt = ENV.fetch("ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT")
+    config.active_record.encryption.primary_key = ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
+    config.active_record.encryption.deterministic_key = ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
+    config.active_record.encryption.key_derivation_salt = ENV["ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT"]
 
     if ENV['RAILS_LOG_TO_STDOUT'].present?
       logger = ActiveSupport::Logger.new($stdout)
